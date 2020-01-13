cbe19f3eab85012b28df9cc1e66326700f0f44f3
Max Bode <maximilianbode@gmx.de>
2020-01-13 17:11:47 +0100

    FontAwesome Brand Icons

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,6 +1,15 @@
+/* eslint-disable sort-imports */
+/* eslint-disable @typescript-eslint/no-unnecessary-type-assertion */
 import { Component } from '@angular/core';
 // eslint-disable-next-line sort-imports
 import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
+// eslint-disable-next-line prettier/prettier
+import {
+    faFacebook,
+    faForumbee,
+    faLinkedin,
+} from '@fortawesome/free-brands-svg-icons';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
 
 /**
  * Komponente f&uuml;r den Footer.
@@ -13,6 +22,10 @@ import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
 export class FooterComponent {
     readonly faEnvelope = faEnvelope;
     readonly faPhone = faPhone;
+    readonly faFacebook = faFacebook as IconProp;
+    readonly faLinkedin = faLinkedin as IconProp;
+    readonly faForumbee = faForumbee as IconProp;
+
     constructor() {
         console.log('FooterComponent.constructor()');
     }
